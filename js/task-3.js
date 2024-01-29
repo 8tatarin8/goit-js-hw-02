2688e18ccfc3c7a8aaa0d9a0b8d2e8adc6752de5
8tatarin8 <mr.tatarenkof@gmail.com>
2024-01-29 19:09:10 +0200

    OFF/task-3/task-4

--- js/task-3.js
+++ js/task-3.js
@@ -17,3 +17,18 @@
 // console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
 //! РІШЕННЯ:
+function checkForSpam(message) {
+  let a = "spam";
+  let b = "sale";
+  forbiddenWords =
+    message.toLowerCase().includes(a.toLowerCase()) ||
+    message.toLowerCase().includes(b.toLowerCase());
+  return forbiddenWords;
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
